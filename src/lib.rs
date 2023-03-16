dce86b5aaa72309c11b82a7bd42d7ff5350c5c0f
MauricinhoMorales <mauricio1999morales@gmail.com>
2023-03-15 23:11:27 -0400

    Revert "[Test] Tried some changes"
    This reverts commit 1ec8aecf91cd9053b31be1c27af958709e3f9fc9.

--- src/lib.rs
+++ src/lib.rs
@@ -125,7 +125,7 @@ pub mod pallet {
         type MaxCID: Get<u32>;
         type Pool: PoolInterface<AccountId = Self::AccountId>;
         type Asset: InterfacePallet<
-            <T as frame_system::Config>::AccountId,
+            &<T as frame_system::Config>::AccountId,
             <T as sugarfunge_asset::pallet::Config>::ClassId,
             <T as sugarfunge_asset::pallet::Config>::AssetId,
             u128,
