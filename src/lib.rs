49a86ebca9fb4b06f7db57f79ac3fb2885bbb2f3
Ubuntu <ubuntu@build-2-powerful-20240321-223827.us-central1-a.c.fula-core.internal>
2024-03-27 16:12:39 +0000

    corrected some errors

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,9 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
+extern crate alloc;
+use alloc::string::String;
+use alloc::format; // Use this for format!
+
 use codec::{Decode, Encode, MaxEncodedLen};
 use frame_support::{dispatch::DispatchResult, ensure, traits::Get, BoundedVec};
 use frame_system::{self as system};
